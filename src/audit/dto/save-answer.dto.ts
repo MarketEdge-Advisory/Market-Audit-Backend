1c714cf3ecc38c51a1e223f3b503d76f6f59dcfc
Hammed-OD <hammed@marketedgeadvisory.com>
2026-03-16 13:44:01 +0100

    added email module, admin module , auth module

--- src/audit/dto/save-answer.dto.ts
+++ src/audit/dto/save-answer.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class SaveAnswerDto {
-  questionId!: string;
-  optionId!: string;
+  @IsString()
+  @IsNotEmpty()
+  questionId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  optionId: string;
 }
